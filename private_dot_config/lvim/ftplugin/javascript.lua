9a5b9cca3b0ff29ea22eadfd6f2245999b1023db
Jocelyn Gaudette <95389546+juicy-g@users.noreply.github.com>
2024-12-20 22:15:28 -0600

    Add diagnostics ignored codes

--- private_dot_config/lvim/ftplugin/javascript.lua
+++ private_dot_config/lvim/ftplugin/javascript.lua
@@ -1,19 +1,4 @@
 require("lvim.lsp.manager").setup("tsserver", {
-  -- handlers = {
-  --   ["textDocument/publishDiagnostics"] = function(_, _, params, client_id, _, config)
-  --     if params.diagnostics ~= nil then
-  --       local idx = 1
-  --       while idx <= #params.diagnostics do
-  --         if params.diagnostics[idx].code == 80001 then
-  --           table.remove(params.diagnostics, idx)
-  --         else
-  --           idx = idx + 1
-  --         end
-  --       end
-  --     end
-  --     vim.lsp.diagnostic.on_publish_diagnostics(_, params, client_id, config)
-  --   end,
-  -- },
   init_options = {
     hostInfo = "neovim",
     preferences = {
@@ -22,5 +7,6 @@ require("lvim.lsp.manager").setup("tsserver", {
   },
   settings = {
     documentFormatting = true,
+    diagnostics = { ignoredCodes = { 80001 } },
   },
 })
