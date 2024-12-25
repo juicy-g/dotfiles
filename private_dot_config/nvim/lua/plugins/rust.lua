28cabd5f34d194131beddb4ada2b3522ccbfd6ec
Jocelyn Gaudette <95389546+juicy-g@users.noreply.github.com>
2024-12-25 17:24:38 -0600

    Improve rust lazy loading

--- private_dot_config/nvim/lua/plugins/rust.lua
+++ private_dot_config/nvim/lua/plugins/rust.lua
@@ -1,5 +1,33 @@
 return {
-  'mrcjkb/rustaceanvim',
-  version = '^5',
-  lazy = false,
+  {
+    "saecki/crates.nvim",
+    event = { "BufRead Cargo.toml" },
+    opts = {},
+  },
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^5",
+    ft = { "rust" },
+    -- lazy = false,
+    opts = {
+      server = {
+        on_attach = function(_, bufnr)
+          vim.keymap.set("n", "<leader>la", function()
+            vim.cmd.RustLsp("codeAction")
+          end, { desc = "Code Action", silent = true, buffer = bufnr })
+          vim.keymap.set(
+            "n",
+            "K",
+            function()
+              vim.cmd.RustLsp({ "hover", "actions" })
+            end,
+            { desc = "Hover", silent = true, buffer = bufnr }
+          )
+        end,
+      }
+    },
+    config = function(_, opts)
+      vim.g.rustaceanvim = vim.tbl_deep_extend("keep", vim.g.rustaceanvim or {}, opts or {})
+    end,
+  }
 }
