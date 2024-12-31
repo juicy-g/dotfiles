947aef78d906e188cd005c2a99dba362741e2791
Jocelyn Gaudette <95389546+juicy-g@users.noreply.github.com>
2024-12-31 13:41:24 -0600

    Add Ruff formatter

--- private_dot_config/nvim/lua/plugins/conform.lua
+++ private_dot_config/nvim/lua/plugins/conform.lua
@@ -1,31 +1,32 @@
 return {
-  "stevearc/conform.nvim",
-  event = "BufWritePre",
-  cmd = "ConformInfo",
-  keys = {
-    {
-      "<leader>lf",
-      function()
-        require("conform").format({ async = true }, function(err)
-          if not err then
-            local mode = vim.api.nvim_get_mode().mode
-            if vim.startswith(string.lower(mode), "v") then
-              vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", true)
-            end
-          end
-        end)
-      end,
-      mode = {"n", "v"},
-      desc = "Format",
-    },
-  },
-  opts = {
-    formatters_by_ft = {
-      lua = { "stylua" },
-      javascript = { "prettierd", "prettier", stop_after_first = true },
-    },
-    default_format_opts = {
-      lsp_format = "fallback",
-    },
-  },
+	"stevearc/conform.nvim",
+	event = "BufWritePre",
+	cmd = "ConformInfo",
+	keys = {
+		{
+			"<leader>lf",
+			function()
+				require("conform").format({ async = true }, function(err)
+					if not err then
+						local mode = vim.api.nvim_get_mode().mode
+						if vim.startswith(string.lower(mode), "v") then
+							vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", true)
+						end
+					end
+				end)
+			end,
+			mode = { "n", "v" },
+			desc = "Format",
+		},
+	},
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			javascript = { "prettierd", "prettier", stop_after_first = true },
+			python = { "ruff_format", "ruff_organize_imports" },
+		},
+		default_format_opts = {
+			lsp_format = "fallback",
+		},
+	},
 }
