25a6a58b4d0e148edb094889f0c3fdb1a9ba5526
Jocelyn Gaudette <95389546+juicy-g@users.noreply.github.com>
2024-12-31 13:45:00 -0600

    Add python to format on save

--- private_dot_config/nvim/lua/config/autocmds.lua
+++ private_dot_config/nvim/lua/config/autocmds.lua
@@ -1,62 +1,62 @@
 vim.api.nvim_create_autocmd("FileType", {
-  desc = "Press q to quit",
-  pattern = {
-    "qf",
-    "help",
-    "man",
-    "floaterm",
-    "lspinfo",
-    "lir",
-    "lsp-installer",
-    "null-ls-info",
-    "tsplayground",
-    "DressingSelect",
-    "Jaq",
-  },
-  callback = function()
-    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = true })
-    vim.opt_local.buflisted = false
-  end,
+	desc = "Press q to quit",
+	pattern = {
+		"qf",
+		"help",
+		"man",
+		"floaterm",
+		"lspinfo",
+		"lir",
+		"lsp-installer",
+		"null-ls-info",
+		"tsplayground",
+		"DressingSelect",
+		"Jaq",
+	},
+	callback = function()
+		vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = true })
+		vim.opt_local.buflisted = false
+	end,
 })
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking text",
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+	desc = "Highlight when yanking text",
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 vim.api.nvim_create_autocmd("BufEnter", {
-  desc = "Restore NvimTree when restoring a session",
-  pattern = "NvimTree*",
-  callback = function()
-    local api = require("nvim-tree.api")
-    local view = require("nvim-tree.view")
+	desc = "Restore NvimTree when restoring a session",
+	pattern = "NvimTree*",
+	callback = function()
+		local api = require("nvim-tree.api")
+		local view = require("nvim-tree.view")
 
-    if not view.is_visible() then
-      api.tree.open()
-    end
-  end,
+		if not view.is_visible() then
+			api.tree.open()
+		end
+	end,
 })
 
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  desc = "Treat all files in chezmoi source directory as chezmoi files",
-  pattern = { os.getenv("HOME") .. "/.local/share/chezmoi/*" },
-  callback = function(args)
-    local bufnr = args.buf
-    local edit_watch = function()
-      require("chezmoi.commands.__edit").watch(bufnr)
-    end
-    vim.schedule(edit_watch)
-  end,
+	desc = "Treat all files in chezmoi source directory as chezmoi files",
+	pattern = { os.getenv("HOME") .. "/.local/share/chezmoi/*" },
+	callback = function(args)
+		local bufnr = args.buf
+		local edit_watch = function()
+			require("chezmoi.commands.__edit").watch(bufnr)
+		end
+		vim.schedule(edit_watch)
+	end,
 })
 
 local Format = vim.api.nvim_create_augroup("Format", { clear = true })
 vim.api.nvim_create_autocmd("BufWritePre", {
-  desc = "Format on save",
-  group = Format,
-  pattern = { "*.lua" },
-  callback = function(args)
-    require("conform").format({ bufnr = args.buf })
-  end,
+	desc = "Format on save",
+	group = Format,
+	pattern = { "*.lua", "*.py" },
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
 })
