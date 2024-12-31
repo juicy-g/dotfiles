e46d13943716ff83d0392dd231e40f2fd3c117f0
Jocelyn Gaudette <95389546+juicy-g@users.noreply.github.com>
2024-12-31 13:43:16 -0600

    Add mason-null-ls

--- private_dot_config/nvim/lua/plugins/mason.lua
+++ private_dot_config/nvim/lua/plugins/mason.lua
@@ -1,12 +1,35 @@
 return {
-  "williamboman/mason.nvim",
-  lazy = true,
-  event = "User FileOpened",
-  build = ":MasonUpdate",
-  cmd = { "Mason", "MasonInstall", "MasonUninstall", "MasonUninstallAll", "MasonLog" },
-  opts = {
-    ui = {
-      border = "rounded",
-    },
-  }
+	{
+		"williamboman/mason.nvim",
+		lazy = true,
+		event = "User FileOpened",
+		build = ":MasonUpdate",
+		cmd = { "Mason", "MasonInstall", "MasonUninstall", "MasonUninstallAll", "MasonLog" },
+		opts = {
+			ui = {
+				border = "rounded",
+			},
+		},
+	},
+	{
+		"jay-babu/mason-null-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"williamboman/mason.nvim",
+			"nvimtools/none-ls.nvim",
+		},
+		config = function()
+			require("mason-null-ls").setup({
+				ensure_installed = { "ruff", "stylua" },
+				methods = {
+					diagnostics = true,
+					formatting = true,
+					code_actions = true,
+					completion = true,
+					hover = true,
+				},
+				handlers = {},
+			})
+		end,
+	},
 }
