7cfcf8343b0875e753b67c806d0cafb4c410606f
Jocelyn Gaudette <95389546+juicy-g@users.noreply.github.com>
2024-12-31 13:42:00 -0600

    Add luv library

--- private_dot_config/nvim/lua/plugins/lazydev.lua
+++ private_dot_config/nvim/lua/plugins/lazydev.lua
@@ -2,6 +2,8 @@ return {
   "folke/lazydev.nvim",
   ft = "lua",
   opts = {
-    -- { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+    library = {
+      { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+    }
   }
 }
