e0c2c30e1596ddc8ec6bb648cb7794e3c7bf855b
Jocelyn Gaudette <95389546+juicy-g@users.noreply.github.com>
2024-12-31 13:43:30 -0600

    Add nvim-notify

---
+++ private_dot_config/nvim/lua/plugins/notifications.lua
@@ -0,0 +1,27 @@
+return {
+  "rcarriga/nvim-notify",
+  init = function()
+    vim.notify = require("notify")
+  end,
+  opts = {
+    background_colour = "NotifyBackground",
+    fps = 30,
+    icons = {
+      DEBUG = "",
+      ERROR = "",
+      INFO = "",
+      TRACE = "✎",
+      WARN = ""
+    },
+    level = 2,
+    minimum_width = 50,
+    render = "default",
+    stages = "fade_in_slide_out",
+    time_formats = {
+      notification = "%T",
+      notification_history = "%FT%T"
+    },
+    timeout = 5000,
+    top_down = true
+  }
+}
