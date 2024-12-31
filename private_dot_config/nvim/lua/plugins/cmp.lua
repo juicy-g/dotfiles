563f34d11e8f7157daba0bff237c2f35a73d70ee
Jocelyn Gaudette <95389546+juicy-g@users.noreply.github.com>
2024-12-31 13:40:54 -0600

    Add symbol for Codeium

--- private_dot_config/nvim/lua/plugins/cmp.lua
+++ private_dot_config/nvim/lua/plugins/cmp.lua
@@ -1,151 +1,139 @@
 return {
-  "hrsh7th/nvim-cmp",
-  event = "InsertEnter",
-  dependencies = {
-    {
-      "saadparwaiz1/cmp_luasnip",
-    },
-    {
-      "L3MON4D3/LuaSnip",
-      dependencies = {
-        "rafamadriz/friendly-snippets",
-      },
-      build = "make install_jsregexp"
-    },
-    {
-      "hrsh7th/cmp-nvim-lsp",
-    },
-    {
-      "hrsh7th/cmp-nvim-lua",
-    },
-    { "hrsh7th/cmp-path",
-    },
-    { "hrsh7th/cmp-cmdline",
-    },
-    { "hrsh7th/cmp-buffer",
-    },
-    { "SergioRibera/cmp-dotenv",
-    },
-    {
-      "onsails/lspkind.nvim",
-    }
-  },
-  config = function()
-    local cmp = require("cmp")
-    local lspkind = require("lspkind")
-    local luasnip = require("luasnip")
-    require("luasnip/loaders/from_vscode").lazy_load()
+	"hrsh7th/nvim-cmp",
+	event = "InsertEnter",
+	dependencies = {
+		{
+			"L3MON4D3/LuaSnip",
+			dependencies = {
+				"rafamadriz/friendly-snippets",
+			},
+			build = "make install_jsregexp",
+		},
+		"saadparwaiz1/cmp_luasnip",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-nvim-lua",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"hrsh7th/cmp-buffer",
+		"SergioRibera/cmp-dotenv",
+		"onsails/lspkind.nvim",
+	},
+	config = function()
+		local cmp = require("cmp")
+		local lspkind = require("lspkind")
+		local luasnip = require("luasnip")
+		require("luasnip/loaders/from_vscode").lazy_load()
 
-    local check_backspace = function()
-      local col = vim.fn.col "." - 1
-      return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
-    end
+		local check_backspace = function()
+			local col = vim.fn.col(".") - 1
+			return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
+		end
 
-    cmp.setup({
-      sources = {
-        {
-          name = "lazydev",
-          group_index = 0,
-        },
-        { name = "nvim_lsp" },
-        { name = "nvim_lua" },
-        { name = "luasnip" },
-        { name = "buffer" },
-        { name = "path" },
-        { name = "codeium" },
-        { name = "dotenv" }
-      },
-      formatting = {
-        format = lspkind.cmp_format({
-          mode = "symbol",
-        }),
-        duplicates = {
-          buffer = 1,
-          path = 1,
-          nvim_lsp = 0,
-          luasnip = 1,
-        },
-        duplicates_default = 0,
-        fields = { "abbr", "kind", "menu" },
-      },
-      confirm_opts = {
-        behavior = cmp.ConfirmBehavior.Replace,
-        select = true,
-      },
-      window = {
-        completion = {
-          completeopt = "menu,menuone,select,preview",
-          border = "rounded",
-          scrollbar = false,
-          winhighlight = "Normal:CmpDocumentation,FloatBorder:CmpDocumentationBorder",
-        },
-        documentation = {
-          border = "rounded",
-        },
-      },
-      mapping = cmp.mapping.preset.insert({
-        ["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
-        ["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
-        ["<Down>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
-        ["<Up>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
-        ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-        ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
-        ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-        ["<C-e>"] = cmp.mapping {
-          i = cmp.mapping.abort(),
-          c = cmp.mapping.close(),
-        },
-        ["<CR>"] = cmp.mapping.confirm { select = true },
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.expandable() then
-            luasnip.expand()
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          elseif check_backspace() then
-            fallback()
-          else
-            fallback()
-          end
-        end, {
-          "i",
-          "s",
-        }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, {
-          "i",
-          "s",
-        }),
-      }),
-      snippet = {
-        expand = function(args)
-          vim.snippet.expand(args.body)
-        end,
-      },
-    })
+		cmp.setup({
+			preselect = "item",
+			completion = {
+				completeopt = "menu,menuone,select,preview",
+			},
+			sources = {
+				{
+					name = "lazydev",
+					group_index = 0,
+				},
+				{ name = "nvim_lsp" },
+				{ name = "nvim_lua" },
+				{ name = "luasnip" },
+				{ name = "buffer" },
+				{ name = "path" },
+				{ name = "codeium" },
+				{ name = "dotenv" },
+			},
+			formatting = {
+				format = lspkind.cmp_format({
+					mode = "symbol",
+					symbol_map = { Codeium = "" },
+				}),
+				duplicates = {
+					buffer = 1,
+					path = 1,
+					nvim_lsp = 0,
+					luasnip = 1,
+				},
+				duplicates_default = 0,
+				fields = { "abbr", "kind", "menu" },
+			},
+			window = {
+				completion = {
+					border = "rounded",
+					scrollbar = false,
+					winhighlight = "Normal:CmpDocumentation,FloatBorder:CmpDocumentationBorder",
+				},
+				documentation = {
+					border = "rounded",
+				},
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+				["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+				["<Down>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+				["<Up>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+				["<C-u>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+				["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+				["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+				["<C-e>"] = cmp.mapping({
+					i = cmp.mapping.abort(),
+					c = cmp.mapping.close(),
+				}),
+				["<CR>"] = cmp.mapping.confirm({ select = true }),
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif luasnip.expandable() then
+						luasnip.expand()
+					elseif luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
+					elseif check_backspace() then
+						fallback()
+					else
+						fallback()
+					end
+				end, {
+					"i",
+					"s",
+				}),
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, {
+					"i",
+					"s",
+				}),
+			}),
+			snippet = {
+				expand = function(args)
+					vim.snippet.expand(args.body)
+				end,
+			},
+		})
 
-    cmp.setup.cmdline({ "/", "?" }, {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = {
-        { name = "buffer" }
-      }
-    })
+		cmp.setup.cmdline({ "/", "?" }, {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = {
+				{ name = "buffer" },
+			},
+		})
 
-    cmp.setup.cmdline(":", {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = cmp.config.sources({
-        { name = "path" },
-        { name = "cmdline" }
-      }),
-      matching = { disallow_symbol_nonprefix_matching = false }
-    })
-  end
+		cmp.setup.cmdline(":", {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = cmp.config.sources({
+				{ name = "path" },
+				{ name = "cmdline" },
+			}),
+			matching = { disallow_symbol_nonprefix_matching = false },
+		})
+	end,
 }
