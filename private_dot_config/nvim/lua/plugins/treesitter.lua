b02403601035cff11a6df852985985b1ee9b9876
Jocelyn Gaudette <95389546+juicy-g@users.noreply.github.com>
2024-12-31 13:44:17 -0600

    Add python to ensure_installed

--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
   event = { "BufReadPost", "BufNewFile" },
   build = ":TSUpdate",
   opts = {
-    ensure_installed = { "lua", "javascript", "typescript", "markdown", "markdown_inline", "bash" },
+    ensure_installed = { "lua", "javascript", "typescript", "markdown", "markdown_inline", "bash", "python" },
     auto_install = true,
     highlight = { enable = true },
     indent = { enable = true },
