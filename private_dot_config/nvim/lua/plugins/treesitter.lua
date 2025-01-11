e7f8e4586fcb43893a3c118a8925350556c19e50
Jocelyn Gaudette <95389546+juicy-g@users.noreply.github.com>
2025-01-11 09:43:02 -0600

    Add nvim-tree-pairs

--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,22 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPost", "BufNewFile" },
-  build = ":TSUpdate",
-  opts = {
-    ensure_installed = { "lua", "javascript", "typescript", "markdown", "markdown_inline", "bash", "python" },
-    auto_install = true,
-    highlight = { enable = true },
-    indent = { enable = true },
-  },
-  config = function(_, opts)
-    require("nvim-treesitter.configs").setup(opts)
-  end
+	{
+		"nvim-treesitter/nvim-treesitter",
+		event = { "BufReadPost", "BufNewFile" },
+		build = ":TSUpdate",
+		opts = {
+			ensure_installed = { "lua", "javascript", "typescript", "markdown", "markdown_inline", "bash", "python" },
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		},
+		config = function(_, opts)
+			require("nvim-treesitter.configs").setup(opts)
+		end,
+	},
+	{
+		"yorickpeterse/nvim-tree-pairs",
+		event = { "BufReadPost", "BufNewFile" },
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		config = true,
+	},
 }
