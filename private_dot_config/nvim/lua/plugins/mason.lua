50f95ed44171cdda3541310efbf4a608cc579aa2
Jocelyn Gaudette <95389546+juicy-g@users.noreply.github.com>
2025-01-11 09:41:45 -0600

    Remove none-ls in favour of conform.nvim

--- private_dot_config/nvim/lua/plugins/mason.lua
+++ private_dot_config/nvim/lua/plugins/mason.lua
@@ -11,25 +11,26 @@ return {
 			},
 		},
 	},
-	{
-		"jay-babu/mason-null-ls.nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			"williamboman/mason.nvim",
-			"nvimtools/none-ls.nvim",
-		},
-		config = function()
-			require("mason-null-ls").setup({
-				ensure_installed = { "ruff", "stylua" },
-				methods = {
-					diagnostics = true,
-					formatting = true,
-					code_actions = true,
-					completion = true,
-					hover = true,
-				},
-				handlers = {},
-			})
-		end,
-	},
+	-- {
+	-- 	"jay-babu/mason-null-ls.nvim",
+	-- 	event = { "BufReadPre", "BufNewFile" },
+	-- 	dependencies = {
+	-- 		"williamboman/mason.nvim",
+	-- 		"nvimtools/none-ls.nvim",
+	-- 	},
+	-- 	config = function()
+	-- 		require("mason-null-ls").setup({
+	-- 			ensure_installed = { "ruff", "stylua", "prettierd" },
+	-- 			methods = {
+	-- 				diagnostics = true,
+	-- 				formatting = true,
+	-- 				code_actions = true,
+	-- 				completion = true,
+	-- 				hover = true,
+	-- 			},
+	-- 			handlers = {},
+	-- 		})
+	-- 		require("null-ls").setup({})
+	-- 	end,
+	-- },
 }
