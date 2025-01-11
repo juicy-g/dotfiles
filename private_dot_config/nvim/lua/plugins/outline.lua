d1e1e013179cfa013fa0c1cca667d4b816cfb1db
Jocelyn Gaudette <95389546+juicy-g@users.noreply.github.com>
2025-01-11 09:40:58 -0600

    Change trouble group to toggles

--- private_dot_config/nvim/lua/plugins/outline.lua
+++ private_dot_config/nvim/lua/plugins/outline.lua
@@ -1,11 +1,11 @@
 return {
-  "stevearc/aerial.nvim",
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "nvim-tree/nvim-web-devicons",
-  },
-  opts = {},
-  keys = {
-    { "<leader>o", "<cmd>AerialToggle<cr>", desc = "Code Outline" },
-  }
+	"stevearc/aerial.nvim",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-tree/nvim-web-devicons",
+	},
+	opts = {},
+	keys = {
+		{ "<leader>to", "<cmd>AerialToggle<cr>", desc = "Toggle Outline" },
+	},
 }
--- private_dot_config/nvim/lua/plugins/whichkey.lua
+++ private_dot_config/nvim/lua/plugins/whichkey.lua
@@ -1,36 +1,50 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  opts = {
-    win = {
-      border = "rounded"
-    },
- sort = { "alphanum", "local", "order", "mod" },
-  },
-  config = function(_, opts)
-    local wk = require("which-key")
-    wk.add({
-      { "<leader>b",     group = "Buffers" },
-      { "<leader>g",     group = "Git" },
-      { "<leader>f",     group = "Find" },
-      { "<leader>l",     group = "LSP" },
-      { "<leader>ld",    vim.diagnostic.open_float,                                    desc = "Open diagnostics" },
-      { "<leader>t",     group = "Trouble" },
-      { "<leader>tD",    "<cmd>Trouble diagnostics toggle<cr>",                        desc = "Workspace diagnostics" },
-      { "<leader>td",    "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",           desc = "Buffer diagnostics" },
-      { "<leader>ts",    "<cmd>Trouble symbols toggle focus=false<cr>",                desc = "Symbols" },
-      { "<leader>tl",    "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", desc = "LSP" },
-      { "<leader>tq",    "<cmd>Trouble qflist toggle<cr>",                             desc = "Quickfix list" },
-      { "<leader>tt",    "<cmd>Trouble todo filter = {tag = {TODO,FIX,FIXME}}<cr>",    desc = "Todo" },
-      { "<leader><tab>", group = "Tabs" },
-      {
-        "<leader>?",
-        function()
-          require("which-key").show({ global = false })
-        end,
-        desc = "Buffer local keymaps"
-      },
-    })
-    wk.setup(opts)
-  end,
+	"folke/which-key.nvim",
+	event = "VimEnter",
+	opts = {
+		win = {
+			border = "rounded",
+		},
+		sort = { "alphanum", "local", "order", "mod" },
+		expand = 1,
+	},
+	config = function(_, opts)
+		local wk = require("which-key")
+		wk.add({
+			{ "<leader>b", group = "Buffers" },
+			{ "<leader>g", group = "Git" },
+			{ "<leader>s", group = "Search" },
+			{ "<leader>l", group = "LSP", icon = "" },
+			{
+				"<leader>ld",
+				vim.diagnostic.open_float,
+				desc = "Open diagnostics",
+			},
+			{ "<leader>qq", "<cmd>qa<cr>", desc = "Quit all" },
+			{ "<leader>t", group = "Toggle" },
+			-- {
+			-- 	"<leader>tD",
+			-- 	"<cmd>Trouble diagnostics toggle<cr>",
+			-- 	desc = "Workspace diagnostics",
+			-- },
+			{
+				"<leader>td",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Toggle buffer diagnostics",
+			},
+			-- { "<leader>ts", "<cmd>Trouble symbols toggle focus=false<cr>", desc = "Symbols" },
+			-- { "<leader>tl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", desc = "LSP" },
+			{ "<leader>tq", "<cmd>Trouble qflist toggle<cr>", desc = "Toggle quickfix" },
+			-- { "<leader>tt", "<cmd>Trouble todo filter = {tag = {TODO,FIX,FIXME}}<cr>", desc = "Todo" },
+			{ "<leader><tab>", group = "Tabs" },
+			{
+				"<leader>b?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer local keymaps",
+			},
+		})
+		wk.setup(opts)
+	end,
 }
