885badec4c7cf6a5752332b406bdab7d3c6b2133
Jocelyn Gaudette <95389546+juicy-g@users.noreply.github.com>
2025-01-11 09:38:16 -0600

    Add more buffers keymaps

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -28,9 +28,19 @@ vim.keymap.set("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
 vim.keymap.set("n", "<S-h>", "<cmd>BufferPrevious<cr>", { desc = "Prev buffer", noremap = true, silent = true })
 vim.keymap.set("n", "<S-l>", "<cmd>BufferNext<cr>", { desc = "Next buffer", noremap = true, silent = true })
 vim.keymap.set("n", "<leader>bc", "<cmd>BufferClose<cr>", { desc = "Close buffer", noremap = true, silent = true })
+vim.keymap.set("n", "<leader>bf", "<cmd>BufferFirst<cr>", { desc = "First buffer", noremap = true, silent = true })
 vim.keymap.set("n", "<leader>bl", "<cmd>BufferLast<cr>", { desc = "Last buffer", noremap = true, silent = true })
 vim.keymap.set("n", "<leader>bp", "<cmd>BufferPin<cr>", { desc = "Pin/Unpin buffer", noremap = true, silent = true })
+vim.keymap.set("n", "<leader>br", "<cmd>BufferRestore<cr>", { desc = "Restore buffer", noremap = true, silent = true })
+vim.keymap.set("n", "<leader>bs", "<cmd>BufferPick<cr>", { desc = "Select buffer", noremap = true, silent = true })
 vim.keymap.set("n", "<leader><space>", "<cmd>e #<cr>", { desc = "Other buffer", noremap = true, silent = true })
+vim.keymap.set(
+	"n",
+	"<A-<>",
+	"<cmd>BufferMovePrevious<cr>",
+	{ desc = "Move buffer left", noremap = true, silent = true }
+)
+vim.keymap.set("n", "<A->>", "<cmd>BufferMoveNext<cr>", { desc = "Move buffer right", noremap = true, silent = true })
 vim.keymap.set("n", "<leader>1", "<cmd>BufferGoto 1<cr>", { desc = "which_key_ignore", noremap = true, silent = true })
 vim.keymap.set("n", "<leader>2", "<cmd>BufferGoto 2<cr>", { desc = "which_key_ignore", noremap = true, silent = true })
 vim.keymap.set("n", "<leader>3", "<cmd>BufferGoto 3<cr>", { desc = "which_key_ignore", noremap = true, silent = true })
@@ -43,11 +53,11 @@ vim.keymap.set("n", "<leader>9", "<cmd>BufferGoto 9<cr>", { desc = "which_key_ig
 
 -- Clear search and stop snippet on escape
 vim.keymap.set({ "i", "n", "s" }, "<esc>", function()
-  vim.cmd("noh")
-  if vim.snippet then
-    vim.snippet.stop()
-  end
-  return "<esc>"
+	vim.cmd("noh")
+	if vim.snippet then
+		vim.snippet.stop()
+	end
+	return "<esc>"
 end, { expr = true, desc = "Escape and clear hlsearch" })
 
 -- Select all
@@ -66,16 +76,15 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 -- Add undo break-points
-vim.keymap.set("i", ",", ",<c-g>u")
-vim.keymap.set("i", ".", ".<c-g>u")
-vim.keymap.set("i", ";", ";<c-g>u")
+-- vim.keymap.set("i", ",", ",<c-g>u")
+-- vim.keymap.set("i", ".", ".<c-g>u")
+-- vim.keymap.set("i", ";", ";<c-g>u")
 
 -- Save file
 vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
 
 -- Quit
 vim.keymap.set({ "i", "x", "n", "s" }, "<C-q>", "<cmd>q<cr>", { desc = "Quit" })
-vim.keymap.set("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all" })
 
 -- Better indenting
 vim.keymap.set("v", "<", "<gv")
@@ -86,19 +95,19 @@ vim.keymap.set("n", "[q", vim.cmd.cprev, { desc = "Previous quickfix" })
 vim.keymap.set("n", "]q", vim.cmd.cnext, { desc = "Next quickfix" })
 
 -- Diagnostics navigation
-local diagnostic_goto = function(next, severity)
-  local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
-  severity = severity and vim.diagnostic.severity[severity] or nil
-  return function()
-    go({ severity = severity })
-  end
-end
-vim.keymap.set("n", "]d", diagnostic_goto(true), { desc = "Next diagnostic" })
-vim.keymap.set("n", "[d", diagnostic_goto(false), { desc = "Prev diagnostic" })
-vim.keymap.set("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next error" })
-vim.keymap.set("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev error" })
-vim.keymap.set("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next warning" })
-vim.keymap.set("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev warning" })
+-- local diagnostic_goto = function(next, severity)
+--   local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
+--   severity = severity and vim.diagnostic.severity[severity] or nil
+--   return function()
+--     go({ severity = severity })
+--   end
+-- end
+-- vim.keymap.set("n", "]d", diagnostic_goto(true), { desc = "Next diagnostic" })
+-- vim.keymap.set("n", "[d", diagnostic_goto(false), { desc = "Prev diagnostic" })
+-- vim.keymap.set("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next error" })
+-- vim.keymap.set("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev error" })
+-- vim.keymap.set("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next warning" })
+-- vim.keymap.set("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev warning" })
 
 -- Tabs
 vim.keymap.set("n", "<leader><tab>l", "<cmd>tablast<cr>", { desc = "Last tab" })
