ca5175876bfa0f18aadabf59db224c7a3725c199
Jocelyn Gaudette <95389546+juicy-g@users.noreply.github.com>
2025-01-11 09:38:27 -0600

    Formatting

--- private_dot_config/nvim/lua/config/lazy.lua
+++ private_dot_config/nvim/lua/config/lazy.lua
@@ -1,13 +1,13 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -15,32 +15,36 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 require("lazy").setup({
-  spec = {
-    { import = "plugins" },
-  },
-  install = {
-    colorscheme = { "tokyonight-storm" },
-  },
-  ui = {
-    border = "rounded",
-    backdrop = 100
-  },
-  change_detection = {
-    enabled = false,
-    notify = false,
-  },
-  performance = {
-    rtp = {
-      disabled_plugins = {
-        "gzip",
-        "matchit",
-        "matchparen",
-        "netrwPlugin",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
-      },
-    },
-  },
+	spec = {
+		{ import = "plugins" },
+	},
+	install = {
+		colorscheme = { "tokyonight-storm" },
+	},
+	ui = {
+		border = "rounded",
+		backdrop = 100,
+	},
+	change_detection = {
+		enabled = false,
+		notify = false,
+	},
+	performance = {
+		rtp = {
+			disabled_plugins = {
+				"gzip",
+				"matchit",
+				"matchparen",
+				"netrwPlugin",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
 })
+
+require("config.autocmds")
+require("config.keymaps")
+require("config.options")
