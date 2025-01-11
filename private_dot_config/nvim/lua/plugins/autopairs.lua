787d1ec42427443e28160b4cde7b031705b5ae84
Jocelyn Gaudette <95389546+juicy-g@users.noreply.github.com>
2025-01-11 09:39:09 -0600

    Add on_confirm_done to autopairs

--- private_dot_config/nvim/lua/plugins/autopairs.lua
+++ private_dot_config/nvim/lua/plugins/autopairs.lua
@@ -1,14 +1,19 @@
 return {
-  {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    opts = {
-      check_ts = true,
-      disable_filetype = { "TelescopePrompt" },
-    }
-  },
-  {
-    "windwp/nvim-ts-autotag",
-    config = true,
-  }
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		opts = {
+			check_ts = true,
+			disable_filetype = { "TelescopePrompt" },
+		},
+		config = function(_, opts)
+			require("nvim-autopairs").setup(opts)
+			require("cmp").event:on("confirm_done", require("nvim-autopairs.completion.cmp").on_confirm_done())
+		end,
+	},
+	{
+		"windwp/nvim-ts-autotag",
+		event = "InsertEnter",
+		config = true,
+	},
 }
