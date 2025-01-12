62ecafead785b61bdc00b4d354372250a3283387
Jocelyn Gaudette <95389546+juicy-g@users.noreply.github.com>
2025-01-12 16:34:58 -0600

    Fix lazy loading

--- private_dot_config/nvim/lua/plugins/diffview.lua
+++ private_dot_config/nvim/lua/plugins/diffview.lua
@@ -1,13 +1,6 @@
--- only load those plugins if the current buffer is a git repository
-local is_inside_git_repo = function()
-  local git_dir = vim.fn.finddir(".git", vim.fn.expand("%:p:h") .. ";")
-  return vim.fn.isdirectory(git_dir) ~= 0
-end
-
 return {
-  "sindrets/diffview.nvim",
-  lazy = true,
-  enabled = is_inside_git_repo(),
-  cmd = "DiffviewOpen",
-  keys = { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Open Git diff" },
+	"sindrets/diffview.nvim",
+	lazy = true,
+	cmd = "DiffviewOpen",
+	keys = { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Open Git diff" },
 }
