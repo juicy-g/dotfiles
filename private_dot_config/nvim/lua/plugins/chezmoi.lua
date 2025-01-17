893edcdf1d0fe983f94328136b31fa6ebb1354a9
Jocelyn Gaudette <95389546+juicy-g@users.noreply.github.com>
2025-01-17 15:33:29 -0600

    Formatting

--- private_dot_config/nvim/lua/plugins/chezmoi.lua
+++ private_dot_config/nvim/lua/plugins/chezmoi.lua
@@ -1,15 +1,15 @@
 return {
-  {
-    "xvzc/chezmoi.nvim",
-    cmd = { "ChezmoiList", "ChezmoiEdit" },
-    dependencies = { "nvim-lua/plenary.nvim" },
-    opts = {},
-  },
-  {
-    "alker0/chezmoi.vim",
-    init = function()
-      vim.g["chezmoi#use_tmp_buffer"] = true
-      vim.g["chezmoi#source_dir_path"] = os.getenv "HOME" .. "/.local/share/chezmoi"
-    end,
-  }
+	{
+		"xvzc/chezmoi.nvim",
+		cmd = { "ChezmoiList", "ChezmoiEdit" },
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {},
+	},
+	{
+		"alker0/chezmoi.vim",
+		init = function()
+			vim.g["chezmoi#use_tmp_buffer"] = true
+			vim.g["chezmoi#source_dir_path"] = os.getenv("HOME") .. "/.local/share/chezmoi"
+		end,
+	},
 }
--- private_dot_config/nvim/lua/plugins/comment.lua
+++ private_dot_config/nvim/lua/plugins/comment.lua
@@ -1,28 +1,28 @@
 return {
-  "numToStr/Comment.nvim",
-  dependencies = {
-    {
-      "JoosepAlviste/nvim-ts-context-commentstring",
-      event = "VeryLazy",
-    },
-  },
-  config = function()
-    local wk = require "which-key"
-    wk.add {
-      { "<leader>/", "<Plug>(comment_toggle_linewise_current)", desc = "Comment Line" },
-    }
+	"numToStr/Comment.nvim",
+	dependencies = {
+		{
+			"JoosepAlviste/nvim-ts-context-commentstring",
+			event = "VeryLazy",
+		},
+	},
+	config = function()
+		local wk = require("which-key")
+		wk.add({
+			{ "<leader>/", "<Plug>(comment_toggle_linewise_current)", desc = "Comment Line", icon = "󰊀" },
+		})
 
-    wk.add {
-      { "<leader>/", "<Plug>(comment_toggle_linewise_visual)", desc = "Comment Line", mode = "v" },
-    }
+		wk.add({
+			{ "<leader>/", "<Plug>(comment_toggle_linewise_visual)", desc = "Comment Line", mode = "v", icon = "󰊀" },
+		})
 
-    vim.g.skip_ts_context_commentstring_module = true
-    require("ts_context_commentstring").setup {
-      enable_autocmd = false,
-    }
+		vim.g.skip_ts_context_commentstring_module = true
+		require("ts_context_commentstring").setup({
+			enable_autocmd = false,
+		})
 
-    require("Comment").setup {
-      pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-    }
-  end
+		require("Comment").setup({
+			pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+		})
+	end,
 }
