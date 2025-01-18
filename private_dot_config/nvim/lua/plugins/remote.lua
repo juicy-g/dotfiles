9bef867be18fb7b907ec8bc7490ad6287a2fd880
Jocelyn Gaudette <95389546+juicy-g@users.noreply.github.com>
2025-01-18 15:19:05 -0600

    Fix missing config plugin spec option

--- private_dot_config/nvim/lua/plugins/remote.lua
+++ private_dot_config/nvim/lua/plugins/remote.lua
@@ -6,19 +6,21 @@ return {
 		"MunifTanjim/nui.nvim",
 		"nvim-telescope/telescope.nvim",
 	},
-	require("remote-nvim").setup({
-		client_callback = function(port)
-			local cmd = ("tmux new -s remote -d 'nvim --server localhost:%s --remote-ui'"):format(port)
-			vim.fn.jobstart(cmd, {
-				detach = true,
-				on_exit = function(job_id, exit_code, event_type)
-					if exit_code == 0 then
-						print("Remote neovim session successfully started")
-					else
-						print("Client", job_id, "exited with code", exit_code, "Event type:", event_type)
-					end
-				end,
-			})
-		end,
-	}),
+	config = function()
+		require("remote-nvim").setup({
+			client_callback = function(port)
+				local cmd = ("tmux new -s remote -d 'nvim --server localhost:%s --remote-ui'"):format(port)
+				vim.fn.jobstart(cmd, {
+					detach = true,
+					on_exit = function(job_id, exit_code, event_type)
+						if exit_code == 0 then
+							print("Remote neovim session successfully started")
+						else
+							print("Client", job_id, "exited with code", exit_code, "Event type:", event_type)
+						end
+					end,
+				})
+			end,
+		})
+	end,
 }
