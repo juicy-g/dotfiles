395afcddffea95ecd61b9986a22ee92c373ead70
Jocelyn Gaudette <95389546+juicy-g@users.noreply.github.com>
2025-05-18 08:43:07 -0500

    Prevent ( for react functional components

--- private_dot_config/nvim/lua/plugins/autopairs.lua
+++ private_dot_config/nvim/lua/plugins/autopairs.lua
@@ -8,7 +8,21 @@ return {
 		},
 		config = function(_, opts)
 			require("nvim-autopairs").setup(opts)
-			require("cmp").event:on("confirm_done", require("nvim-autopairs.completion.cmp").on_confirm_done())
+			local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+			local cmp = require('cmp')
+
+			cmp.event:on('confirm_done', function(args)
+				local line = vim.api.nvim_get_current_line()
+
+				-- prevent adding parentheses when importing functional components (eg. <Foo)
+				local is_component_import = line:match('</?%s*[%w_]+.-/?>?')
+
+				if is_component_import then
+					return false
+				end
+
+				cmp_autopairs.on_confirm_done()(args)
+			end)
 		end,
 	},
 	{
