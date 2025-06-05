3b933ddd8b3b8dc26dd3179066e03cc22770faa3
Jocelyn Gaudette <95389546+juicy-g@users.noreply.github.com>
2025-06-05 15:55:31 -0500

    Add codecompanion.nvim

---
+++ private_dot_config/nvim/lua/plugins/ai.lua
@@ -0,0 +1,69 @@
+return {
+  "olimorris/codecompanion.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    {
+      "Davidyz/VectorCode",
+      version = "*",
+      build = "pipx upgrade vectorcode",
+      dependencies = { "nvim-lua/plenary.nvim" },
+    },
+  },
+  init = function()
+    vim.cmd([[cab cc CodeCompanion]])
+  end,
+  config = function()
+    local spinner = require("config.spinner")
+    spinner:init()
+
+    require("codecompanion").setup({
+      strategies = {
+        chat = {
+          adapter = "ollama",
+          keymaps = {
+            send = {
+              callback = function(chat)
+                vim.cmd("stopinsert")
+                chat:submit()
+                chat:add_buf_message({ role = "llm", content = "" })
+              end,
+              index = 1,
+              description = "Send",
+            },
+            close = {
+              modes = {
+                n = "q",
+              },
+              index = 3,
+              callback = "keymaps.close",
+              description = "Close Chat",
+            },
+            stop = {
+              modes = {
+                n = "<C-c>",
+              },
+              index = 4,
+              callback = "keymaps.stop",
+              description = "Stop Request",
+            },
+          },
+        },
+        inline = {
+          adapter = "ollama",
+        },
+        agent = {
+          adapter = "ollama",
+        },
+      },
+      extensions = {
+        vectorcode = {
+          opts = { add_tool = true, add_slash_command = true, tool_opts = {} },
+        },
+      },
+      opts = {
+        log_level = "ERROR", -- TRACE|DEBUG|ERROR|INFO
+      },
+    })
+  end
+}
