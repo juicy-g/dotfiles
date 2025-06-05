ccbcb3f545bd3fa4bfd946ab26a199da1b6fa845
Jocelyn Gaudette <95389546+juicy-g@users.noreply.github.com>
2025-06-05 15:58:21 -0500

    Change backdrop to be transparent

--- private_dot_config/nvim/lua/plugins/mason.lua
+++ private_dot_config/nvim/lua/plugins/mason.lua
@@ -8,29 +8,8 @@ return {
 		opts = {
 			ui = {
 				border = "rounded",
+				backdrop = 100
 			},
 		},
 	},
-	-- {
-	-- 	"jay-babu/mason-null-ls.nvim",
-	-- 	event = { "BufReadPre", "BufNewFile" },
-	-- 	dependencies = {
-	-- 		"williamboman/mason.nvim",
-	-- 		"nvimtools/none-ls.nvim",
-	-- 	},
-	-- 	config = function()
-	-- 		require("mason-null-ls").setup({
-	-- 			ensure_installed = { "ruff", "stylua", "prettierd" },
-	-- 			methods = {
-	-- 				diagnostics = true,
-	-- 				formatting = true,
-	-- 				code_actions = true,
-	-- 				completion = true,
-	-- 				hover = true,
-	-- 			},
-	-- 			handlers = {},
-	-- 		})
-	-- 		require("null-ls").setup({})
-	-- 	end,
-	-- },
 }
