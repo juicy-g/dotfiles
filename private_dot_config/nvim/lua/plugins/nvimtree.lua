db200b091e8ff4bdcda1aefa7f9c4db708f97ef3
Jocelyn Gaudette <95389546+juicy-g@users.noreply.github.com>
2025-06-05 15:57:11 -0500

    Add nvim-tree-preview

--- private_dot_config/nvim/lua/plugins/nvimtree.lua
+++ private_dot_config/nvim/lua/plugins/nvimtree.lua
@@ -4,9 +4,44 @@ return {
 		version = "*",
 		dependencies = {
 			"nvim-tree/nvim-web-devicons",
+			{
+				"b0o/nvim-tree-preview.lua",
+				dependencies = {
+					"nvim-lua/plenary.nvim"
+				}
+			}
 		},
 		config = function()
-			require("nvim-tree").setup({})
+			require("nvim-tree").setup({
+				on_attach = function(bufnr)
+					local api = require("nvim-tree.api")
+
+					api.config.mappings.default_on_attach(bufnr)
+
+					local function opts(desc)
+						return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+					end
+
+					local preview = require("nvim-tree-preview")
+
+					vim.keymap.set("n", "P", preview.watch, opts "Preview (Watch)")
+					vim.keymap.set("n", "<Esc>", preview.unwatch, opts "Close Preview/Unwatch")
+					vim.keymap.set("n", "<C-f>", function() return preview.scroll(4) end, opts "Scroll Down")
+					vim.keymap.set("n", "<C-b>", function() return preview.scroll(-4) end, opts "Scroll Up")
+
+					vim.keymap.set("n", "<Tab>", function()
+						local ok, node = pcall(api.tree.get_node_under_cursor)
+						if ok and node then
+							if node.type == "directory" then
+								api.node.open.edit()
+							else
+								preview.node(node, { toggle_focus = true })
+							end
+						end
+					end, opts "Preview")
+				end,
+			})
+
 			local nvimTreeFocusOrToggle = function()
 				local nvimTree = require("nvim-tree.api")
 				local currentBuf = vim.api.nvim_get_current_buf()
