03dee264514ccd07f6e798e13533e662dc6ebbd7
Jocelyn Gaudette <95389546+juicy-g@users.noreply.github.com>
2025-06-05 15:59:14 -0500

    Add colorful-menu.nvim and fix commandline completion menu

--- private_dot_config/nvim/lua/plugins/cmp.lua
+++ private_dot_config/nvim/lua/plugins/cmp.lua
@@ -17,6 +17,7 @@ return {
 		"hrsh7th/cmp-buffer",
 		"SergioRibera/cmp-dotenv",
 		"onsails/lspkind.nvim",
+		"xzbdmw/colorful-menu.nvim",
 	},
 	config = function()
 		local cmp = require("cmp")
@@ -48,10 +49,23 @@ return {
 				{ name = "dotenv" },
 			},
 			formatting = {
-				format = lspkind.cmp_format({
-					mode = "symbol",
-					symbol_map = { Codeium = "" },
-				}),
+				format = function(entry, vim_item)
+					local kind = lspkind.cmp_format({
+						mode = "symbol_text",
+						symbol_map = { Codeium = "" },
+					})(entry, vim.deepcopy(vim_item))
+					local highlights_info = require("colorful-menu").cmp_highlights(entry)
+
+					if highlights_info ~= nil then
+						vim_item.abbr_hl_group = highlights_info.highlights
+						vim_item.abbr = highlights_info.text
+					end
+					local strings = vim.split(kind.kind, "%s", { trimempty = true })
+					vim_item.kind = " " .. (strings[1] or "") .. " "
+					vim_item.menu = ""
+
+					return vim_item
+				end,
 				duplicates = {
 					buffer = 1,
 					path = 1,
@@ -151,6 +165,9 @@ return {
 		})
 
 		cmp.setup.cmdline(":", {
+			completion = {
+				completeopt = "menu,menuone,noselect"
+			},
 			mapping = cmp.mapping.preset.cmdline({
 				["<CR>"] = {
 					c = function()
