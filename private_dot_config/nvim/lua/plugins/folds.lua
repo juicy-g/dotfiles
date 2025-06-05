09c2cad7c9c1d4faf003cb01e237ac8508bb03ad
Jocelyn Gaudette <95389546+juicy-g@users.noreply.github.com>
2025-06-05 15:55:52 -0500

    Add nvim-ufo for folds

---
+++ private_dot_config/nvim/lua/plugins/folds.lua
@@ -0,0 +1,35 @@
+return {
+  "kevinhwang91/nvim-ufo",
+  dependencies = "kevinhwang91/promise-async",
+  event = "BufRead",
+  keys = {
+    { "K", function()
+      local winid = require("ufo").peekFoldedLinesUnderCursor()
+      if not winid then
+        vim.lsp.buf.hover()
+      end
+    end }
+  },
+  init = function()
+    vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+    vim.o.foldcolumn = "0"
+    vim.o.foldlevel = 99
+    vim.o.foldlevelstart = 99
+    vim.o.foldenable = true
+  end,
+  opts = {
+    preview = {
+      win_config = {
+        border = "rounded",
+        winhighlight = "Normal:Folded",
+        winblend = 0,
+      },
+      mappings = {
+        scrollU = "<C-u>",
+        scrollD = "<C-d>",
+        jumpTop = "[",
+        jumpBot = "]",
+      },
+    }
+  }
+}
