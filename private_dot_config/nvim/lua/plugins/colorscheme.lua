70917352d90820e27f7f67b16ed650ca065bc6b4
Jocelyn Gaudette <95389546+juicy-g@users.noreply.github.com>
2025-09-07 09:31:55 -0500

    Cleanup code in colorscheme

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,29 +1,23 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      style = "storm",
-      styles = {
-        floats = "normal",
-        sidebars = "normal",
-      },
-      on_colors = function(colors)
-        colors.bg_statusline = colors.bg
-      end,
-      on_highlights = function(hl, colors)
-        hl.CmpDocumentation = { fg = colors.fg, bg = "#24283b" }
-        hl.CmpDocumentationBorder = { fg = colors.fg, bg = "#24283b" }
-        hl.TelescopePromptBorder = hl.FloatBorder
-      end,
-      plugins = {
-        barbar = true,
-        notify = true,
-      }
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {
+    style = "storm",
+    styles = {
+      floats = "normal",
+      sidebars = "normal",
     },
-    init = function()
-      vim.cmd.colorscheme("tokyonight-storm")
+    on_colors = function(colors)
+      colors.bg_statusline = colors.bg
+    end,
+    on_highlights = function(hl, colors)
+      hl.CmpDocumentation = { fg = colors.fg, bg = "#24283b" }
+      hl.CmpDocumentationBorder = { fg = colors.fg, bg = "#24283b" }
+      hl.TelescopePromptBorder = hl.FloatBorder
     end,
   },
+  init = function()
+    vim.cmd.colorscheme("tokyonight-storm")
+  end
 }
