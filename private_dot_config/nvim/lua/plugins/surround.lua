37fc5ffde9f1d426835c152de09a2744fc3db840
Jocelyn Gaudette <95389546+juicy-g@users.noreply.github.com>
2025-10-13 13:28:24 -0500

    Remove empty lines around when deleting surrounds

--- private_dot_config/nvim/lua/plugins/surround.lua
+++ private_dot_config/nvim/lua/plugins/surround.lua
@@ -3,7 +3,30 @@ return {
 		"kylechui/nvim-surround",
 		version = "*",
 		event = "VeryLazy",
-		opts = {},
+		opts = {
+			move_cursor = "sticky",
+			indent_lines = function(start, stop)
+				local b = vim.bo
+				-- Only re-indent the selection if a formatter is set up already
+				if start < stop and (b.equalprg ~= "" or b.indentexpr ~= "" or b.cindent or b.smartindent or b.lisp) then
+					vim.cmd(string.format("silent normal! %dG=%dG", start, stop))
+					require("nvim-surround.cache").set_callback("")
+				end
+
+				if start < stop then
+					local function clear_line(line) -- strip trailing whitespaces and delete line if empty
+						local l, c = string.gsub(vim.api.nvim_buf_get_lines(0, line - 1, line, false)[1], "%s+$", "")
+						if l == "" then
+							vim.api.nvim_buf_set_lines(0, line - 1, line, false, {})
+						elseif c ~= 0 then
+							vim.api.nvim_buf_set_lines(0, line - 1, line, false, { l })
+						end
+					end
+					clear_line(stop)
+					clear_line(start)
+				end
+			end,
+		},
 	},
 	{
 		dir = "~/repos/surround-ui.nvim",
