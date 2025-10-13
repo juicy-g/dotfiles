4ecfd80665ad951d00c2e7d0dc594b2e6fca32f8
Jocelyn Gaudette <95389546+juicy-g@users.noreply.github.com>
2025-10-13 13:31:19 -0500

    Make lualine bold

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -8,6 +8,7 @@ return {
       floats = "normal",
       sidebars = "normal",
     },
+    lualine_bold = true,
     on_colors = function(colors)
       colors.bg_statusline = colors.bg
     end,
@@ -15,6 +16,8 @@ return {
       hl.CmpDocumentation = { fg = colors.fg, bg = "#24283b" }
       hl.CmpDocumentationBorder = { fg = colors.fg, bg = "#24283b" }
       hl.TelescopePromptBorder = hl.FloatBorder
+      hl.NvimTreeWinSeparator = hl.WinSeparator
+      hl.BufferTabpageFill = { bg = colors.bg }
     end,
   },
   init = function()
