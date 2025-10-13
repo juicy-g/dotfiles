012b1e659b9484643e890b0dd8557eef57a94b73
Jocelyn Gaudette <95389546+juicy-g@users.noreply.github.com>
2025-10-13 13:23:05 -0500

    Formatting

--- private_dot_config/nvim/lua/plugins/dashboard.lua
+++ private_dot_config/nvim/lua/plugins/dashboard.lua
@@ -17,13 +17,13 @@ return {
     }
 
     dashboard.section.buttons.val = {
-      dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("e", "  > New file", "<cmd>ene <BAR> startinsert <cr>"),
       dashboard.button("d", "  > Dotfiles", "<cmd>Telescope chezmoi find_files<cr>"),
       dashboard.button("f", "󰈞  > Find file", ":Telescope find_files<CR>"),
       dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
       dashboard.button("s", "󱥬  > Session", "<cmd>lua require('persistence').load()<cr>"),
       dashboard.button("t", "  > Text", "<cmd>Telescope live_grep<cr>"),
-      dashboard.button("q", "󰈆  > Quit", ":qa<CR>"),
+      dashboard.button("q", "󰈆  > Quit", "<cmd>qa<cr>"),
     }
 
     dashboard.opts = {
--- private_dot_config/nvim/lua/plugins/persistence.lua
+++ private_dot_config/nvim/lua/plugins/persistence.lua
@@ -1,10 +1,10 @@
 return {
-  {
-    "folke/persistence.nvim",
-    event = "BufReadPre",
-    opts = {
-      options = { "buffers", "curdir", "tabpages", "folds", "globals", "options", "resize", "terminal", "winpos", "winsize" },
-      pre_save = function() vim.api.nvim_exec_autocmds("User", { pattern = "SessionSavePre" }) end,
-    }
-  },
+  "folke/persistence.nvim",
+  event = "BufReadPre",
+  opts = {
+    options = { "buffers", "curdir", "tabpages", "folds", "globals", "options", "resize", "winpos", "winsize" },
+    pre_save = function()
+      vim.api.nvim_exec_autocmds("User", { pattern = "SessionSavePre" })
+    end,
+  }
 }
