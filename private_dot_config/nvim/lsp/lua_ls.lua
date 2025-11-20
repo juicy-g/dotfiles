3ab55a4982bab70c0c441b58cc4404b1c0adf174
Jocelyn Gaudette <95389546+juicy-g@users.noreply.github.com>
2025-11-20 14:33:49 -0500

    Remove lazydev.nvim

--- private_dot_config/nvim/lsp/lua_ls.lua
+++ private_dot_config/nvim/lsp/lua_ls.lua
@@ -1,7 +1,32 @@
-return {
-  cmd = { "lua-language-server" },
-  filetypes = { "lua" },
-  root_markers = { ".luarc.json", ".luarc.jsonc", ".luacheckrc", ".stylua.toml", "stylua.toml", "selene.toml", "selene.yml", ".git" },
+vim.lsp.config("lua_ls", {
+  on_init = function(client)
+    if client.workspace_folders then
+      local path = client.workspace_folders[1].name
+      if
+          path ~= vim.fn.stdpath("config")
+          and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
+      then
+        return
+      end
+    end
+
+    client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+      runtime = {
+        version = "LuaJIT",
+        path = {
+          "lua/?.lua",
+          "lua/?/init.lua",
+        },
+      },
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME,
+          "${3rd}/luv/library"
+        }
+      }
+    })
+  end,
   settings = {
     Lua = {
       telemetry = {
@@ -16,9 +41,6 @@ return {
       diagnostics = {
         disable = { "undefined-global", "missing-fields" },
       },
-      workspace = {
-        checkThirdParty = false,
-      },
       completion = {
         callSnippet = "Replace",
       },
@@ -29,7 +51,14 @@ return {
           indent_size = "2",
           quote_style = "double",
         },
-      },
-    },
+      }
+    }
   }
+})
+
+---@type vim.lsp.Config
+return {
+  cmd = { "lua-language-server" },
+  filetypes = { "lua" },
+  root_markers = { ".luarc.json", ".luarc.jsonc", ".luacheckrc", ".stylua.toml", "stylua.toml", "selene.toml", "selene.yml", ".git" },
 }
