43064f38bed6188675a1621dbcf0a46d381cc1bd
Jocelyn Gaudette <95389546+juicy-g@users.noreply.github.com>
2026-07-05 10:45:12 -0500

    Add cursortab.nvim and rename codeium.nvim to windsurf.nvim

--- private_dot_config/nvim/lua/plugins/codeium.lua
+++ private_dot_config/nvim/lua/plugins/codeium.lua
@@ -1,11 +1,44 @@
 return {
-    "Exafunction/codeium.nvim",
+  -- {
+  --   "BlinkResearchLabs/blink-edit.nvim",
+  --   config = function()
+  --     require("blink-edit").setup({
+  --       llm = {
+  --         provider = "sweep",
+  --         backend = "ollama",
+  --         url = "http://localhost:11434",
+  --         model = "sweepai/sweep-next-edit",
+  --       },
+  --     })
+  --   end,
+  -- },
+  {
+    "cursortab/cursortab.nvim",
+    lazy = false,
+    build = "cd server && go build",
+    config = function()
+      require("cursortab").setup({
+        provider = {
+          -- type = "zeta-2",
+          type = "sweep",
+          url = "http://localhost:8888",
+        },
+      })
+    end,
+  },
+  {
+    "Exafunction/windsurf.nvim",
     event = "VeryLazy",
     dependencies = {
-        "nvim-lua/plenary.nvim",
-        "hrsh7th/nvim-cmp",
+      "nvim-lua/plenary.nvim",
+      "hrsh7th/nvim-cmp",
     },
     config = function()
-        require("codeium").setup({})
+      require("codeium").setup({
+        virtual_text = {
+          enabled = false
+        }
+      })
     end
+  }
 }
