e91a4c696c0b0850516d006b9c89ba0237f8385a
Jocelyn Gaudette <95389546+juicy-g@users.noreply.github.com>
2026-07-05 10:45:56 -0500

    Add rule for trailing commas in lua tables

--- private_dot_config/nvim/lua/plugins/autopairs.lua
+++ private_dot_config/nvim/lua/plugins/autopairs.lua
@@ -4,18 +4,17 @@ return {
 		event = "InsertEnter",
 		opts = {
 			check_ts = true,
-			disable_filetype = { "TelescopePrompt" },
 		},
 		config = function(_, opts)
 			require("nvim-autopairs").setup(opts)
-			local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-			local cmp = require('cmp')
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			local cmp = require("cmp")
 
-			cmp.event:on('confirm_done', function(args)
+			cmp.event:on("confirm_done", function(args)
 				local line = vim.api.nvim_get_current_line()
 
-				-- prevent adding parentheses when importing functional components (eg. <Foo)
-				local is_component_import = line:match('</?%s*[%w_]+.-/?>?')
+				-- Prevent adding parentheses when importing functional components (eg. <Foo)
+				local is_component_import = line:match("</?%s*[%w_]+.-/?>?")
 
 				if is_component_import then
 					return false
@@ -23,6 +22,24 @@ return {
 
 				cmp_autopairs.on_confirm_done()(args)
 			end)
+
+			local npairs = require "nvim-autopairs"
+			local Rule = require("nvim-autopairs.rule")
+			local cond = require("nvim-autopairs.conds")
+			local ts_conds = require("nvim-autopairs.ts-conds")
+
+			-- Add trailing commas to "'} inside Lua tables
+			npairs.add_rules({
+				Rule("{", "},", "lua")
+						:with_pair(cond.not_after_regex(","))
+						:with_pair(ts_conds.is_ts_node({ "table_constructor" })),
+				Rule("'", "',", "lua")
+						:with_pair(cond.not_after_regex(","))
+						:with_pair(ts_conds.is_ts_node({ "table_constructor" })),
+				Rule('"', '",', "lua")
+						:with_pair(cond.not_after_regex(","))
+						:with_pair(ts_conds.is_ts_node({ "table_constructor" })),
+			})
 		end,
 	},
 	{
